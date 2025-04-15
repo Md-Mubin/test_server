b3da8dde29ba32269a7dbcc593c3631389bd6cf3
Md Mubin <fontdevmubin@gmail.com>
2025-04-15 14:23:01 +0600

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const router = require("./router")
 
 app.use(express.json())
 app.use(cors({
-    origin : "https://codano-p.vercel.app"
+    origin : "https://codano-p.vercel.app",
+    credentials : true,
+    methods : ["GET", "POST"]
 }))
 app.use(router)
 
